0e20027f21c0fcdb9576f88548b42d14e0555bb2
Sphinx <young_2017@126.com>
2023-11-29 00:42:35 +0800

    [Reading Notes][TCP/IP, Socket programming]
    book name :
        TCP/IP 网络编程 by [韩]伊圣雨
    Reading progress:
        chapter: update 10,11,12/25

---
+++ reading_notes/tcpip_network_programming/chap10/signal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+void timeout(int sig) {
+    if (sig == SIGALRM)
+        puts("Time out");
+    alarm(2);
+}
+
+void keycontrol(int sig) {
+    if (sig == SIGINT)
+        puts("CTRL+C pressed");
+}
+
+int main(int argc, char *argv[]) {
+
+    int i;
+    signal(SIGALRM, timeout);
+    signal(SIGINT, keycontrol);
+
+    for (i = 0; i < 3; i++) {
+        puts("wait ...");
+        sleep(100);
+    }
+
+    return 0;
+}
